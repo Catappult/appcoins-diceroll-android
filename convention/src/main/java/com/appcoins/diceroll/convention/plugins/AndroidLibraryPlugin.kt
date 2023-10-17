8d3f684198a89e9cb7863faaa23486631ee9f94e
danfbfern <danielfernando.1608@gmail.com>
2023-10-17 18:48:22 +0100

    [APT-3599] cleanup kapt and libs

--- convention/src/main/java/com/appcoins/diceroll/convention/plugins/AndroidLibraryPlugin.kt
+++ convention/src/main/java/com/appcoins/diceroll/convention/plugins/AndroidLibraryPlugin.kt
@@ -16,7 +16,6 @@ class AndroidLibraryPlugin : Plugin<Project> {
       with(pluginManager) {
         apply("com.android.library")
         apply("kotlin-android")
-        apply("kotlin-kapt")
         apply<HiltPlugin>()
       }
 
