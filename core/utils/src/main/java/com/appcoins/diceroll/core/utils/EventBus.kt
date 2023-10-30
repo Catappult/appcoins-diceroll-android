dd732cd848bec5b3a082ef2f7a4edbf1d0441f2c
danfbfern <danielfernando.1608@gmail.com>
2023-10-30 17:23:18 +0000

    [APT-3644] update EventBus to allow for multiple tries

--- core/utils/src/main/java/com/appcoins/diceroll/core/utils/EventBus.kt
+++ core/utils/src/main/java/com/appcoins/diceroll/core/utils/EventBus.kt
@@ -1,16 +1,30 @@
 package com.appcoins.diceroll.core.utils
 
+import android.util.Log
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.onCompletion
 
 /**
  * Simple event bus using Kotlin coroutines and channels to publish and listen to events.
  */
 object EventBus {
 
-  val eventChannel = Channel<Any>()
+  /**
+   * The channel used for publishing and listening to events. It is recreated upon completion.
+   */
+  var eventChannel = Channel<Any>()
+
+  /**
+   * Creates a new event channel with a CONFLATED buffer type.
+   *
+   * @return A new channel for handling events with a CONFLATED buffer type.
+   */
+  fun createEventChannel(): Channel<Any> {
+    return Channel(Channel.CONFLATED)
+  }
 
   /**
    * Publishes an event to the event bus, so that all listeners can receive it.
@@ -18,6 +32,7 @@ object EventBus {
    * @param event The event to be published.
    */
   suspend fun publish(event: Any) {
+    Log.d(CUSTOM_TAG, "EventBus: publish: event $event")
     eventChannel.send(event)
   }
 
@@ -28,6 +43,10 @@ object EventBus {
    * @return A Flow of events of the specified type [T].
    */
   inline fun <reified T> listen(): Flow<T> {
-    return eventChannel.consumeAsFlow().filterIsInstance()
+    Log.d(CUSTOM_TAG, "EventBus: listen: T ${T::class.java}")
+    return eventChannel.consumeAsFlow().filterIsInstance<T>()
+      .onCompletion {
+        eventChannel = createEventChannel()
+      }
   }
 }
