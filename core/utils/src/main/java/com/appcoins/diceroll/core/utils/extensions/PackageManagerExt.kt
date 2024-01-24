263e07e557635a2b1b9e932271d20b58dcb9ffa6
danfbfern <danielfernando.1608@gmail.com>
2024-01-24 14:01:55 +0000

    [APT-3812] create extension functions for installer info

---
+++ core/utils/src/main/java/com/appcoins/diceroll/core/utils/extensions/PackageManagerExt.kt
@@ -0,0 +1,28 @@
+package com.appcoins.diceroll.core.utils.extensions
+
+import android.content.pm.PackageManager
+import android.os.Build
+import com.appcoins.diceroll.core.utils.diceRollPackage
+
+fun PackageManager.getInstallerInfo(): String? {
+  return try {
+    val installerPackageName = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+      this.getInstallSourceInfo(diceRollPackage).installingPackageName
+    else
+      this.getInstallerPackageName(diceRollPackage)
+
+    installerPackageName?.takeIf { isAppInstalled(it) }
+  } catch (e: Exception) {
+    e.printStackTrace()
+    null
+  }
+}
+
+fun PackageManager.isAppInstalled(packageName: String): Boolean {
+  return try {
+    getPackageInfo(packageName, PackageManager.GET_META_DATA)
+    true
+  } catch (e: PackageManager.NameNotFoundException) {
+    false
+  }
+}
