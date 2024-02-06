f9fa6e3dba6d76219bfee932efc7c71313ca9f9b
danfbfern <danielfernando.1608@gmail.com>
2024-02-06 15:45:32 +0000

    update billing to 0.7.1.0 and app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
   namespace = "com.appcoins.diceroll"
   defaultConfig {
     applicationId = "com.appcoins.diceroll"
-    versionCode = 8
-    versionName = "0.4.1"
+    versionCode = 9
+    versionName = "0.4.2"
   }
 }
 
