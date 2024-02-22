16d081ed70ff375c14d217292c8c9175f672ee16
danfbfern <danielfernando.1608@gmail.com>
2024-02-22 12:25:57 +0000

    updates osp intent package handler, to include gameshub as an option

--- payments/appcoins-osp/src/main/java/com/appcoins/diceroll/payments/appcoins/osp/OspManager.kt
+++ payments/appcoins-osp/src/main/java/com/appcoins/diceroll/payments/appcoins/osp/OspManager.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import com.appcoins.diceroll.core.utils.gamesHubPackage
 import com.appcoins.diceroll.core.utils.walletPackage
 import com.appcoins.diceroll.payments.appcoins.osp.data.repository.OspRepository
 import kotlinx.coroutines.CoroutineScope
@@ -39,11 +40,11 @@ class OspManager @Inject constructor(private val ospRepository: OspRepository) {
     callback: OspLaunchCallback
   ) {
     runCatching {
-      val intent = Intent(Intent.ACTION_VIEW)
-      intent.data = Uri.parse(ospUrl)
-
-      if (isAppCoinsWalletInstalled(context)) {
-        intent.setPackage(walletPackage)
+      val intent = Intent(Intent.ACTION_VIEW).apply {
+        data = Uri.parse(ospUrl)
+        getSupportedServicePackage(context)?.let { packageName ->
+          setPackage(packageName)
+        }
       }
       (context as Activity).startActivity(intent)
       callback.onSuccess(orderReference = Result.success(ospOrderReference))
@@ -52,10 +53,13 @@ class OspManager @Inject constructor(private val ospRepository: OspRepository) {
     }
   }
 
-  private fun isAppCoinsWalletInstalled(context: Context): Boolean {
-    val packageManager = (context as Activity).packageManager
-    return runCatching {
-      packageManager.getPackageInfo(walletPackage, PackageManager.GET_ACTIVITIES)
-    }.isSuccess
+  private fun getSupportedServicePackage(context: Context): String? {
+    val packageManager = context.packageManager
+    val packagesToCheck = listOf(walletPackage, gamesHubPackage)
+    return packagesToCheck.firstOrNull { packageName ->
+      runCatching {
+        packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
+      }.isSuccess
+    }
   }
 }
