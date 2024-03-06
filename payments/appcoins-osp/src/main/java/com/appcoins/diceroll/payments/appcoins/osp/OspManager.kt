803bffd9b4223428788e59de77636e7a2b52172b
danfbfern <danielfernando.1608@gmail.com>
2024-03-06 01:28:22 +0000

    added resolveActivity check for the action intent

--- payments/appcoins-osp/src/main/java/com/appcoins/diceroll/payments/appcoins/osp/OspManager.kt
+++ payments/appcoins-osp/src/main/java/com/appcoins/diceroll/payments/appcoins/osp/OspManager.kt
@@ -40,26 +40,36 @@ class OspManager @Inject constructor(private val ospRepository: OspRepository) {
     callback: OspLaunchCallback
   ) {
     runCatching {
-      val intent = Intent(Intent.ACTION_VIEW).apply {
-        data = Uri.parse(ospUrl)
-        getSupportedServicePackage(context)?.let { packageName ->
+      val intent = Intent(Intent.ACTION_VIEW, Uri.parse(ospUrl)).apply {
+        getSupportedServicePackage(context, ospUrl)?.let { packageName ->
           setPackage(packageName)
         }
+        addCategory(Intent.CATEGORY_BROWSABLE)
       }
-      (context as Activity).startActivity(intent)
+      (context as Activity).startActivityForResult(intent, 10003)
       callback.onSuccess(orderReference = Result.success(ospOrderReference))
     }.onFailure {
       callback.onError(error = Result.failure(it))
     }
   }
 
-  private fun getSupportedServicePackage(context: Context): String? {
-    val packageManager = context.packageManager
+  private fun getSupportedServicePackage(context: Context, ospUrl: String): String? {
+    val packageManager = (context as Activity).packageManager
     val packagesToCheck = listOf(walletPackage, gamesHubPackage)
-    return packagesToCheck.firstOrNull { packageName ->
-      runCatching {
+    return packagesToCheck.find { packageName ->
+      val isPackageInstalled = runCatching {
         packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
       }.isSuccess
+
+      if (isPackageInstalled) {
+        val intentForCheck = Intent(Intent.ACTION_VIEW).apply {
+          data = Uri.parse(ospUrl)
+          `package` = packageName
+        }
+        intentForCheck.resolveActivity(packageManager) != null
+      } else {
+        false
+      }
     }
   }
 }
