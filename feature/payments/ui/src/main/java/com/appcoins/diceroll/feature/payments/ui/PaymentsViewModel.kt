cd75091bbb00f4de5a00bc313a9839b854ca48be
Sergiy Khomyn <sergiy.khomyn@aptoide.com>
2024-04-19 12:27:01 +0100

    Revert code indentation.

--- feature/payments/ui/src/main/java/com/appcoins/diceroll/feature/payments/ui/PaymentsViewModel.kt
+++ feature/payments/ui/src/main/java/com/appcoins/diceroll/feature/payments/ui/PaymentsViewModel.kt
@@ -28,94 +28,92 @@ import kotlin.time.Duration.Companion.minutes
 
 @HiltViewModel
 class PaymentsViewModel @Inject constructor(
-    private val resetAttemptsUseCase: ResetAttemptsUseCase,
-    private val pollOspCallbackUseCase: PollOspCallbackUseCase,
-    val savedStateHandle: SavedStateHandle,
+  private val resetAttemptsUseCase: ResetAttemptsUseCase,
+  private val pollOspCallbackUseCase: PollOspCallbackUseCase,
+  val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
-    private val itemId = savedStateHandle.get<String>(DestinationArgs.ItemId)
-    private val attempts = savedStateHandle.get<String>(DestinationArgs.AttemptsLeft)
+  private val itemId = savedStateHandle.get<String>(DestinationArgs.ItemId)
+  private val attempts = savedStateHandle.get<String>(DestinationArgs.AttemptsLeft)
 
-    private val _paymentOptionsState =
-        MutableStateFlow<PaymentsOptionsUiState>(PaymentsOptionsUiState.Loading)
-    internal val paymentOptionsState: StateFlow<PaymentsOptionsUiState> get() = _paymentOptionsState
+  private val _paymentOptionsState =
+    MutableStateFlow<PaymentsOptionsUiState>(PaymentsOptionsUiState.Loading)
+  internal val paymentOptionsState: StateFlow<PaymentsOptionsUiState> get() = _paymentOptionsState
 
-    init {
-        when {
-            itemId == null || attempts == null -> {
-                _paymentOptionsState.value = PaymentsOptionsUiState.Error
+  init {
+    when {
+      itemId == null || attempts == null -> {
+        _paymentOptionsState.value = PaymentsOptionsUiState.Error
+      }
+
+      attempts == DEFAULT_ATTEMPTS_NUMBER.toString() -> {
+        _paymentOptionsState.value = PaymentsOptionsUiState.NotAvailable
+      }
+
+      else -> {
+        _paymentOptionsState.value = PaymentsOptionsUiState.Available(itemId)
+      }
+    }
+  }
+
+  private val _paymentResultState =
+    MutableStateFlow<PaymentsResultUiState>(PaymentsResultUiState.Initialized)
+  internal val paymentResultState: StateFlow<PaymentsResultUiState> get() = _paymentResultState
+
+
+  fun setPaymentResult(paymentsIntegration: PaymentsIntegration) {
+    _paymentOptionsState.value = PaymentsOptionsUiState.OptionSelected
+    _paymentResultState.value = PaymentsResultUiState.Loading
+    when (paymentsIntegration) {
+      is PaymentsIntegration.OSP -> {
+        observeOspCallback(paymentsIntegration.orderReference)
+      }
+
+      is PaymentsIntegration.SDK -> {
+        observeSdkResult()
+      }
+    }
+  }
+
+  private fun observeOspCallback(orderReference: Result<String>) {
+    orderReference.fold(
+      onSuccess = { reference ->
+        pollOspCallbackUseCase(reference)
+          .map { callbackResult ->
+            when (callbackResult.status) {
+              OspCallbackState.COMPLETED -> PaymentsResultUiState.Success
+              OspCallbackState.CANCELED -> PaymentsResultUiState.UserCanceled
+              OspCallbackState.FAILED -> PaymentsResultUiState.Failed
+              OspCallbackState.PENDING -> PaymentsResultUiState.Loading
             }
+          }
+          .onEach { paymentResultUiState -> _paymentResultState.value = paymentResultUiState }
+          .takeUntilTimeout(10.minutes)
+          .catch {
+            _paymentResultState.value = PaymentsResultUiState.Failed
+          }
+          .produceIn(viewModelScope)
+      },
+      onFailure = {
+        _paymentResultState.value = PaymentsResultUiState.Failed
+      }
+    )
+  }
 
-            attempts == DEFAULT_ATTEMPTS_NUMBER.toString() -> {
-                _paymentOptionsState.value = PaymentsOptionsUiState.NotAvailable
-            }
-
-            else -> {
-                _paymentOptionsState.value = PaymentsOptionsUiState.Available(itemId)
-            }
+  private fun observeSdkResult() {
+    viewModelScope.launch {
+        PurchaseResultStream.listen<PurchaseResponse>().collect {
+        val paymentState = when (it.responseCode) {
+            ResponseCode.OK.value -> PaymentsResultUiState.Success
+            ResponseCode.USER_CANCELED.value -> PaymentsResultUiState.UserCanceled
+          else -> PaymentsResultUiState.Failed
         }
+        _paymentResultState.value = paymentState
+      }
     }
+  }
 
-    private val _paymentResultState =
-        MutableStateFlow<PaymentsResultUiState>(PaymentsResultUiState.Initialized)
-    internal val paymentResultState: StateFlow<PaymentsResultUiState> get() = _paymentResultState
-
-
-    fun setPaymentResult(paymentsIntegration: PaymentsIntegration) {
-        _paymentOptionsState.value = PaymentsOptionsUiState.OptionSelected
-        _paymentResultState.value = PaymentsResultUiState.Loading
-        when (paymentsIntegration) {
-            is PaymentsIntegration.OSP -> {
-                observeOspCallback(paymentsIntegration.orderReference)
-            }
-
-            is PaymentsIntegration.SDK -> {
-                observeSdkResult()
-            }
-        }
-    }
-
-    private fun observeOspCallback(orderReference: Result<String>) {
-        orderReference.fold(
-            onSuccess = { reference ->
-                pollOspCallbackUseCase(reference)
-                    .map { callbackResult ->
-                        when (callbackResult.status) {
-                            OspCallbackState.COMPLETED -> PaymentsResultUiState.Success
-                            OspCallbackState.CANCELED -> PaymentsResultUiState.UserCanceled
-                            OspCallbackState.FAILED -> PaymentsResultUiState.Failed
-                            OspCallbackState.PENDING -> PaymentsResultUiState.Loading
-                        }
-                    }
-                    .onEach { paymentResultUiState ->
-                        _paymentResultState.value = paymentResultUiState
-                    }
-                    .takeUntilTimeout(10.minutes)
-                    .catch {
-                        _paymentResultState.value = PaymentsResultUiState.Failed
-                    }
-                    .produceIn(viewModelScope)
-            },
-            onFailure = {
-                _paymentResultState.value = PaymentsResultUiState.Failed
-            }
-        )
-    }
-
-    private fun observeSdkResult() {
-        viewModelScope.launch {
-            PurchaseResultStream.listen<PurchaseResponse>().collect {
-                val paymentState = when (it.responseCode) {
-                    ResponseCode.OK.value -> PaymentsResultUiState.Success
-                    ResponseCode.USER_CANCELED.value -> PaymentsResultUiState.UserCanceled
-                    else -> PaymentsResultUiState.Failed
-                }
-                _paymentResultState.value = paymentState
-            }
-        }
-    }
-
-    suspend fun resetAttemptsLeft() {
-        resetAttemptsUseCase()
-    }
+  suspend fun resetAttemptsLeft() {
+    resetAttemptsUseCase()
+  }
 }
